1d0c7c397285eb5b0b93b5370d9ccde7a2249c9e
David Barragán Merino <david.barragan@kaleidos.net>
2017-03-14 15:04:14 +0100

    Release a new version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog #
 
 
+## 3.1.1 (2017-03-14)
+- Fix a problem with a malformed gitlab call url
+
 ## 3.1.0 (2017-03-09)
 - Initial version. Adapt from github auth
 
--- back/taiga_contrib_gitlab_auth/__init__.py
+++ back/taiga_contrib_gitlab_auth/__init__.py
@@ -15,6 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = (3, 1, 0)
+__version__ = (3, 1, 1)
 
 default_app_config = "taiga_contrib_gitlab_auth.apps.TaigaContribGitlabAuthAppConfig"
