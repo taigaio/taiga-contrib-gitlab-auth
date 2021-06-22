dc9302a395c1bd04ff2207ae4214bd79d61663ba
David Barragán Merino <david.barragan@kaleidos.net>
2021-06-22 12:07:25 +0200

    (chore) bump to 6.2.1 version

--- back/taiga_contrib_gitlab_auth/__init__.py
+++ back/taiga_contrib_gitlab_auth/__init__.py
@@ -5,6 +5,6 @@
 #
 # Copyright (c) 2021-present Kaleidos Ventures SL
 
-__version__ = (6, 2, 0)
+__version__ = (6, 2, 1)
 
 default_app_config = "taiga_contrib_gitlab_auth.apps.TaigaContribGitlabAuthAppConfig"
