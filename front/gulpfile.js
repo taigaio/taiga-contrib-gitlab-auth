eeb2d5ece73091f11984151abf46236163ac86f8
Juanfran <juanfran.ag@gmail.com>
2023-02-20 15:10:46 +0100

    chore: upgrade to node 16

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -11,50 +11,50 @@ var $ = require('gulp-load-plugins')();
 var merge = require('merge-stream');
 
 var paths = {
-    jade: 'partials/*.jade',
-    coffee: 'coffee/*.coffee',
-    images: 'images/**/*',
-    dist: 'dist/'
+  jade: 'partials/*.jade',
+  coffee: 'coffee/*.coffee',
+  images: 'images/**/*',
+  dist: 'dist/'
 };
 
-gulp.task('copy-config', function() {
-    return gulp.src('gitlab-auth.json')
-        .pipe(gulp.dest(paths.dist));
+gulp.task('copy-config', function () {
+  return gulp.src('gitlab-auth.json')
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('copy-images', function() {
-    return gulp.src(paths.images)
-        .pipe(gulp.dest(paths.dist + "images"));
+gulp.task('copy-images', function () {
+  return gulp.src(paths.images)
+    .pipe(gulp.dest(paths.dist + "images"));
 });
 
-gulp.task('compile', function() {
-    var jade = gulp.src(paths.jade)
-        .pipe($.plumber())
-        .pipe($.cached('jade'))
-        .pipe($.jade({pretty: true}))
-        .pipe($.angularTemplatecache({
-            transformUrl: function(url) {
-                return '/plugins/gitlab-auth/' + url;
-            }
-        }))
-        .pipe($.remember('jade'));
+gulp.task('compile', function () {
+  var jade = gulp.src(paths.jade)
+    .pipe($.plumber())
+    .pipe($.cached('jade'))
+    .pipe($.jade({ pretty: true }))
+    .pipe($.angularTemplatecache({
+      transformUrl: function (url) {
+        return '/plugins/gitlab-auth/' + url;
+      }
+    }))
+    .pipe($.remember('jade'));
 
-    var coffee = gulp.src(paths.coffee)
-        .pipe($.plumber())
-        .pipe($.cached('coffee'))
-        .pipe($.coffee())
-        .pipe($.remember('coffee'));
+  var coffee = gulp.src(paths.coffee)
+    .pipe($.plumber())
+    .pipe($.cached('coffee'))
+    .pipe($.coffee())
+    .pipe($.remember('coffee'));
 
-    return merge(jade, coffee)
-        .pipe($.concat('gitlab-auth.js'))
-        .pipe($.uglify({mangle:false, preserveComments: false}))
-        .pipe(gulp.dest(paths.dist));
+  return merge(jade, coffee)
+    .pipe($.concat('gitlab-auth.js'))
+    .pipe($.uglify({ mangle: false, annotations: false }))
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('watch', function() {
-    gulp.watch([paths.jade, paths.coffee, paths.images], ['copy-images', 'compile']);
+gulp.task('watch', function () {
+  gulp.watch([paths.jade, paths.coffee, paths.images], gulp.parallel('copy-images', 'compile'));
 });
 
-gulp.task('default', ['copy-config', 'copy-images', 'compile', 'watch']);
+gulp.task('default', gulp.series('copy-config', 'copy-images', 'compile', 'watch'));
 
-gulp.task('build', ['copy-config', 'copy-images', 'compile', ]);
+gulp.task('build', gulp.series('copy-config', 'copy-images', 'compile'));
